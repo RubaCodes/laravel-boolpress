382179edf64501eb9fb6a4e7bab0cbbd2465f80c
RubaCodes <rubacodes@gmail.com>
2022-07-27 15:11:55 +0200

    aggiunit post per dare contenuto al blog

--- public/js/front.js
+++ public/js/front.js
@@ -19165,7 +19165,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/albertomarcon/Boolean/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /home/albertomarcon/Boolean/laravel-boolpress/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
